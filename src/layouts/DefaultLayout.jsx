6f597fd4b6ed03325a6560c1360104d2ef02677b
Joohong <joohong.tan@vainsight.com>
2025-02-11 15:53:15 +0800

    fix mobile

--- src/layouts/DefaultLayout.jsx
+++ src/layouts/DefaultLayout.jsx
@@ -3,7 +3,7 @@ import {Container} from "react-bootstrap";
 
 const DefaultLayout = () => {
     return (
-        <Container  className="p-2 p-md-1">
+        <Container  className="p-4 p-sm-1">
             <main className="main p-2 p-md-5">
                 <Outlet />  {/* Content from routes goes here */}
             </main>
