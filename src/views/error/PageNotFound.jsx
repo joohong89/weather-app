2ae3e64f20f92e6f2db8ffe7f0cb0195b214f680
Joohong <joohong.tan@vainsight.com>
2025-02-11 15:20:17 +0800

    updated error page

--- src/views/error/PageNotFound.jsx
+++ src/views/error/PageNotFound.jsx
@@ -1,9 +1,16 @@
+import {Container} from "react-bootstrap";
+import {Link} from "react-router";
+
 const PageNotFound = () => {
     return (
-        <div className="page-not-found">
-            <img src="/images/11.png" alt=""/>
-            Oops, page is not found
-        </div>
+        <Container className="pageNotFound" fluid>
+            <div className="page-not-found d-flex flex-column align-items-center">
+                <img src="/images/11.png" width="300" alt="Not found image"/>
+                <span>Oops, page is not found</span>
+                <Link to={'/'} className="return-link">Back</Link>
+            </div>
+        </Container>
+
     )
 }
 export default PageNotFound
